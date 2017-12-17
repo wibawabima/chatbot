b56ced868bcf0f275a03fee4442c925214550f42
wibawabima <bimanurwibawa@gmail.com>
2017-12-17 12:32:13 +0700

    setting proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,13 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    // protected $proxies;
+    protected $proxies = '**';
+
+    // protected $proxies = [
+    //     '192.168.1.1',
+    //     '192.168.1.2',
+    // ];
 
     /**
      * The current proxy header mappings.
@@ -20,10 +26,15 @@ class TrustProxies extends Middleware
      * @var array
      */
     protected $headers = [
-        Request::HEADER_FORWARDED => 'FORWARDED',
-        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        // Request::HEADER_FORWARDED => 'FORWARDED',
+        // Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        // Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        // Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        // Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        Request::HEADER_FORWARDED => null,
+        Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
+        Illuminate\Http\Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
+        Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
+        Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
     ];
 }
